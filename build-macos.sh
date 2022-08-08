6c7ee66fac783cea5d0e5f8b5e220bf3ceb616c7
gogins <michael.gogins@gmail.com>
2022-08-08 12:40:04 -0400

    #1...

--- build-macos.sh
+++ build-macos.sh
@@ -6,5 +6,6 @@ cd build-macos
 rm -f CMakeCache.txt
 cmake -Wno-dev .. -DCMAKE_PREFIX_PATH=/usr/local:/usr 
 make -j6 VERBOSE=1
+sudo cpack -V -G ZIP
 sudo make install
 echo "Finished building all for macOS."
