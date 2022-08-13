d703ada1aa920633b7ec499fc05dd8355e4050c5
gogins <michael.gogins@gmail.com>
2022-08-12 20:18:31 -0400

    Trying to get MSVC to compile stuff written on Linux/BSD; it was originally written on Windows! (#1).

--- CsoundAC/Platform.hpp
+++ CsoundAC/Platform.hpp
@@ -30,17 +30,10 @@
 #endif
 
 #ifdef _MSC_VER
-#include <../include/limits.h>
-#if _MSC_VER >= 1900
-#include <../ucrt/stdlib.h>
-#else
-#include <../include/stdlib.h>
-#endif
 #ifndef PATH_MAX
 #define PATH_MAX _MAX_PATH
 #endif
-#else
-#include <limits.h>
 #endif
+
 #endif
 
