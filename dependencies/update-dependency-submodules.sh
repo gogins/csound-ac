f6daddc27597944b6b62f385a16cdaae298eec35
gogins <michael.gogins@gmail.com>
2022-08-15 14:39:08 -0400

    Oops, missed the Python package intallation (#1). Also made eigen a submodule as easier to maintain.

--- dependencies/update-dependency-submodules.sh
+++ dependencies/update-dependency-submodules.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
-echo "Updating all submodules for csound-extended..."
+echo "Updating all submodules for csound-ac..."
 git submodule update --init --recursive --remote
 git submodule update --recursive
 git submodule status --recursive
-echo "Finished updating all submodules for csound-extended."
+echo "Finished updating all submodules for csound-ac."
