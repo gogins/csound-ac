e10b8b99920ebc4b236b0833ff65ed95456bfa5f
gogins <michael.gogins@gmail.com>
2022-09-21 22:03:20 -0400

    playpen.py now does all patch testing, much improved (#3).

--- playpen/playpen.py
+++ playpen/playpen.py
@@ -77,10 +77,12 @@ export to various formats.
 
 '''
 import configparser
+import ctcsound
 import datetime
 import os
 import os.path
 import platform
+import random
 import string
 import subprocess
 import sys
@@ -146,6 +148,7 @@ print('Copyright:              ', str_copyright)
 print('Licence:                ', metadata_license)
 print('Publisher:              ', metadata_publisher)
 print('Notes:                  ', metadata_notes)
+print('Output filename:        ', output_filename)
 print('Master filename:        ', master_filename)
 print('Spectrogram filename:   ', spectrogram_filename)
 print('CD quality filename:    ', cd_quality_filename)
@@ -404,6 +407,162 @@ def cpp_astyle():
     finally:
         print("man_csound: {}.".format(source_filepath))
         return
+        
+def generate_csd(patch_filepath, instrument_name, output):
+    csd = '''<CsoundSynthesizer>
+<CsLicense>
+Csound patch test program.
+Copyright (c) 2022 by Michael Gogins.
+
+This file is licensed under the terms of the GNU Lesser Public License version 
+2.1.
+</CsLicense>
+<CsOptions>
+-d -RWf -m163 -+msg_color=0 --midi-key=4 --midi-velocity=5 -Fmisty.mid --sample-accurate -o{output}
+</CsOptions>
+<CsInstruments>
+sr = 48000
+ksmps = 128
+nchnls = 2
+nchnls_i = 1
+0dbfs = 1
+
+// These must be initialized here to be in scope for both 
+// the note and the audio patches.
+
+//gi_Organteq vstinit "/home/mkg/Organteq\ 1/x86-64bit/Organteq\ 1.so", 0
+//vstinfo gi_Organteq 
+
+//gi_Fluidsynth fluidEngine 0, 0
+//gi_FluidSteinway fluidLoad "/home/mkg/Dropbox/Steinway_C.sf2", gi_Fluidsynth, 1
+//fluidProgramSelect gi_Fluidsynth, 0, gi_FluidSteinway, 0, 1
+
+//gi_Pianoteq vstinit "/home/mkg/Pianoteq\ 7/x86-64bit/Pianoteq\ 7.so", 0
+//vstinfo gi_Pianoteq 
+
+alwayson "PianoOutFluidsynth"
+alwayson "PianoOutPianoteq"
+alwayson "OrganOutOrganteq"
+
+{patch_text}
+
+// Comment out if you don't have the Steinway_C.sf2 SoundFont.
+//#include "PianoOutFluidsynth.inc"
+
+// Comment out if you don't have the Pianoteq and vst4cs.
+//#include "PianoOutPianoteq.inc"
+
+// Comment out if you don't have the Organtec and vst4cs.
+//#include "OrganOutOrganteq.inc"
+
+#include "MasterOutput.inc"
+
+iampdbfs init 1
+prints "Default amplitude at 0 dBFS: %9.4f\\n", iampdbfs
+idbafs init dbamp(iampdbfs)
+prints "dbA at 0 dBFS:               %9.4f\\n", idbafs
+iheadroom init 6
+prints "Headroom (dB):               %9.4f\\n", iheadroom
+idbaheadroom init idbafs - iheadroom
+prints "dbA at headroom:             %9.4f\\n", idbaheadroom
+iampheadroom init ampdb(idbaheadroom)
+prints "Amplitude at headroom:       %9.4f\\n", iampheadroom
+prints "Balance so the 'overall amps' at the end of performance is -6 dBFS.\\n"
+prints "nchnls:                      %9.4f\\n", nchnls
+
+connect "{instrument_name}", "outleft", "MasterOutput", "inleft"
+connect "{instrument_name}", "outright", "MasterOutput", "inright"
+connect "PianoOutFluidsynth", "outleft", "MasterOutput", "inleft"
+connect "PianoOutFluidsynth", "outright", "MasterOutput", "inright"
+connect "PianoOutPianoteq", "outleft", "MasterOutput", "inleft"
+connect "PianoOutPianoteq", "outright", "MasterOutput", "inright"
+connect "OrganOutOrganteq", "outleft", "MasterOutput", "inleft"
+connect "OrganOutOrganteq", "outright", "MasterOutput", "inright"
+
+alwayson "MasterOutput"
+
+</CsInstruments>
+<CsScore>
+{standard_score}
+</CsScore>
+</CsoundSynthesizer>
+'''
+    with open(patch_filepath, "r") as file:
+        patch_text = file.read()
+    standard_score = generate_standard_score()
+    csd = csd.format(patch_text=patch_text, instrument_name=instrument_name, output=output, standard_score=standard_score)
+    return csd
+def generate_standard_score():
+    score = 'f 0 60\n'
+    time_= 30.0
+    # Generate a chord.
+    score += 'i  1    %9.4f %9.4f %9.4f %9.4f 0 0.5\n' % (time_, 5, 36, 70)
+    score += 'i  1    %9.4f %9.4f %9.4f %9.4f 0 0.5\n' % (time_, 5, 48, 70)
+    score += 'i  1    %9.4f %9.4f %9.4f %9.4f 0 0.5\n' % (time_, 5, 55, 70)
+    score += 'i  1    %9.4f %9.4f %9.4f %9.4f 0 0.5\n' % (time_, 5, 59, 70)
+    score += 'i  1    %9.4f %9.4f %9.4f %9.4f 0 0.5\n' % (time_, 5, 64, 70)
+    time_ += 7
+    # Generate a random pattern.
+    for note_i in range(50):
+        duration = random.choice([0.125, 0.33334, 0.25, 0.66667, 1])
+        time_ += duration / 2;
+        key = random.choice(range(24, 112, 1))
+        velocity = random.choice([80, 80-6, 80-12, 80-18]) / 3
+        score += 'i  1    %9.4f %9.4f %9.4f %9.4f 0 0.5\n' % (time_, duration, key, 60)
+    time_ += 2
+    # Generate notes to calibrate loudness.
+    for duration in [0.125, .25, 2]:
+        for key in range(24,108,3):
+            time_ = time_ + duration * 1.5
+            velocity = random.choice([80, 80-6, 80-12, 80-18])
+            score += 'i  1    %9.4f %9.4f %9.4f %9.4f 0 0.5\n' % (time_, duration, key, velocity)
+    time_ += 4
+    # Test score-driven note-on, note-off.
+    score += 'i  1.01 %9.4f %9.4f %9.4f %9.4f 0 0.5\n' % (time_, -1, 36, 70)
+    time_ += 1
+    score += 'i  1.02 %9.4f %9.4f %9.4f %9.4f 0 0.5\n' % (time_, -1, 48, 70)
+    time_ += 1
+    score += 'i  1.03 %9.4f %9.4f %9.4f %9.4f 0 0.5\n' % (time_, -1, 55, 70)
+    time_ += 1
+    score += 'i  1.04 %9.4f %9.4f %9.4f %9.4f 0 0.5\n' % (time_, -1, 59, 70)
+    time_ += 1
+    score += 'i  1.05 %9.4f %9.4f %9.4f %9.4f 0 0.5\n' % (time_, -1, 64, 70)
+
+    time_ += 5
+    score += 'i -1.01 %9.4f %9.4f %9.4f %9.4f 0 0.5\n' % (time_, 0, 36, 70)
+    time_ += 1
+    score += 'i -1.02 %9.4f %9.4f %9.4f %9.4f 0 0.5\n' % (time_, 0, 48, 70)
+    time_ += 1
+    score += 'i -1.03 %9.4f %9.4f %9.4f %9.4f 0 0.5\n' % (time_, 0, 55, 70)
+    time_ += 1
+    score += 'i -1.04 %9.4f %9.4f %9.4f %9.4f 0 0.5\n' % (time_, 0, 59, 70)
+    time_ += 1
+    score += 'i -1.05 %9.4f %9.4f %9.4f %9.4f 0 0.5\n' % (time_, 0, 64, 70)
+    return score        
+        
+def csd_patch():
+    try:
+        print("csd_patch: {}...".format(source_filepath))
+        print(sys.argv)
+        patch_filename = source_filepath
+        patch_name = title
+        output = output_filename
+        message_level = 1 + 2 + 32 + 128
+        csound = ctcsound.Csound()
+        csound.message("Patch file: {} Patch name: {} Output: {}\n".format(patch_filename, patch_name, output))
+        csd = generate_csd(patch_filename, patch_name, output)
+        print(csd)
+        csound.compileCsdText(csd)
+        csound.start()
+        csound.perform()
+
+        
+        
+    except:
+        traceback.print_exc()
+    finally:
+        print("csd_patch: {}.".format(source_filepath))
+        return
 
 if command == 'csd-audio':
     csd_audio()
@@ -440,4 +599,8 @@ if command == 'man-csoundac':
     man_csoundac()
 if command == 'cpp-astyle':
     cpp_astyle()
+if command == 'csd-patch':
+    csd_patch()
+    post_process()
+    play()
 
