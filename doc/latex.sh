953676db23ff03a919bfa9fcd62e991fedbde15f
gogins <michael.gogins@gmail.com>
2024-04-13 19:11:31 -0400

    Changing latex setup.

--- doc/latex.sh
+++ doc/latex.sh
@@ -2,6 +2,6 @@
 echo "Building csound-ac.pdf reference manual..."
 doxygen
 cd latex
-pdflatex refman.tex
-pdflatex refman.tex
-mv -f refman.pdf ../../csound-ac.pdf
+pdflatex --jobname=csound-ac refman.tex
+pdflatex --jobname=csound-ac refman.tex
+# mv -f refman.pdf ../../csound-ac.pdf
