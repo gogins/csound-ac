04a9900094a09149a73a927dedc132ac66726610
gogins <michael.gogins@gmail.com>
2025-02-14 21:08:23 +0100

    Finishing up examples for User Guide (#35).

--- clean-build-linux.sh
+++ clean-build-linux.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "Began cleaning and rebuilding all Linux artifacts from `pwd`...."
-sudo apt remove csound-extended-dev
+sudo apt remove csound-ac
 sudo rm -rfd build-linux
+sudo rm -rfd dependencies/libmusicxml/build/lib/*
+sudo rm -rfd dependencies/libmusicxml/build/bin/*
 sudo find . -wholename "*_pycache_*" -delete
 sudo -k
 bash build-linux.sh "$@"
