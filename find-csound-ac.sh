f64c342b19caefa32f9ccdff5299e3d279dcb7e7
gogins <michael.gogins@gmail.com>
2025-03-28 13:50:04 +0100

    Prepare to merge (#45).

--- find-csound-ac.sh
+++ find-csound-ac.sh
@@ -1,2 +1,2 @@
 #/bin/bash
-find / -type f \( -name "*CsoundAC*.so*" -o -name "*CsoundAC*.dylib" -o -name "*CsoundAC*.a" -o -name "CsoundAC.py" \) 2>/dev/null
+find / -type f \( -name "*CsoundAC*.so*" -o -name "*CsoundAC*.dylib" -o -name "*CsoundAC*.a" -o -name "CsoundAC.py" \) $1 2>/dev/null
