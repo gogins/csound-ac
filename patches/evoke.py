5b70ef791bc4e9a6d8ace1c1ed488e0505d49555
gogins <michael.gogins@gmail.com>
2025-11-16 09:49:55 +0100

    Fixed i-time assignment to k_kernel_duration.

--- patches/evoke.py
+++ patches/evoke.py
@@ -9,8 +9,7 @@ during which  ii-V-I chord progression is eoked from the source.
 import sys
 import ctcsound
 csound = ctcsound.Csound()
-csound.compileCsdText('''
-<CsoundSynthesizer>
+csd = '''<CsoundSynthesizer>
 <CsOptions>
 -m163 -RWdfo evoke_harmony_convolution_test.wav 
 </CsOptions>
@@ -28,9 +27,10 @@ ga_input_left      init 0
 ga_input_right     init 0
 ga_mix_left        init 0
 ga_mix_right       init 0
+gi_fade init 1
                       
 instr Noise
-  k_env linsegr 0, 1, p3, 1, 0
+  k_env linsegr 0, gi_fade, p3, gi_fade, 0
   a_source rand 0.3
   a_source tone a_source, 10000
   a_source *= k_env
@@ -40,7 +40,7 @@ instr Noise
 endin
                       
 instr Saw
-  k_env linsegr 0, 1, p3, 1, 0
+  k_env linsegr 0, gi_fade, p3, gi_fade, 0
   k_amplitude = 0.3
   ; Frequency ramps from start of fade-in to end of fade-out
   k_hz line 32.70319783, p3, 523.2511307
@@ -53,24 +53,24 @@ instr Saw
 endin
                                    
 instr Evoker
-  i_fade_in_seconds   = p4
-  i_fade_out_seconds  = p5
-  k_kernel_duration = p6
-  i_impulse_gain = p7
-  i_dirac_level  = p8
-  i_pitch_class_1 = p9
-  i_pitch_class_2 = p10
-  i_pitch_class_3 = p11
-  i_pitch_class_4 = p12
-  i_pitch_class_5 = p13
+  k_kernel_duration init p4
+  i_impulse_gain init p5
+  i_dirac_level  init p6
+  i_pitch_class_1 init p7
+  i_pitch_class_2 init p8
+  i_pitch_class_3 init p9
+  i_pitch_class_4 init p12
+  i_pitch_class_5 init p13
   a_source_left  = ga_input_left
   a_source_right = ga_input_right
   a_processed_left evoke_harmony_convolution a_source_left, k_kernel_duration, i_impulse_gain, i_dirac_level, i_pitch_class_1, i_pitch_class_2, i_pitch_class_3, i_pitch_class_4, i_pitch_class_5   
   a_processed_right evoke_harmony_convolution a_source_right, k_kernel_duration, i_impulse_gain, i_dirac_level, i_pitch_class_1, i_pitch_class_2, i_pitch_class_3, i_pitch_class_4, i_pitch_class_5   
-  k_envelope linsegr 0, i_fade_in_seconds, p3, i_fade_out_seconds, 0
+  k_envelope linsegr 0, gi_fade, p3, gi_fade, 0
   ga_mix_left  += a_processed_left  * k_envelope
   ga_mix_right += a_processed_right * k_envelope
+  ; prints "Evoker: p1 %f p2 %f p3 %f direct p4  %f impulse gain %f dirac level %f\\n", p1, p2, p3, p4, i_impulse_gain, i_dirac_level
   prints "Evoker: p1 %f p2 %f p3 %f kernel dur %f impulse gain %f dirac level %f\\n", p1, p2, p3, k_kernel_duration, i_impulse_gain, i_dirac_level
+
 endin
 
 instr MasterOutput
@@ -81,21 +81,19 @@ instr MasterOutput
   ga_mix_right = 0
 endin                   
 
-instr ExitNow
-    exitnow
-endin 
 </CsInstruments>
 </CsoundSynthesizer>
-''')
+'''   
+
 '''
 a_output evoke_harmony_convolution k_kernel_duration, i_impulse_gain, 
   i_dirac_level, i_pitch_class_1, i_pitch_class_2, i_pitch_class_3, 
   i_pitch_class_4, i_pitch_class_5
 '''
-k_kernel_durations = [.1, 1, 5]
-i_impulse_gains = [.5, .25, .125]
-i_dirac_levels = [.5, .25, .125]
-source_instruments = [1, 2]
+k_kernel_durations = [.01, .1, 1]
+i_impulse_gains = [.125, .25, .75]
+i_dirac_levels = [.75, .5, .25]
+source_instruments = [2, 1]
 chords = []
 chords.append([2, 5, 9, 12])   
 chords.append([7, 11, 14, 17])  
@@ -115,7 +113,7 @@ def generate_score():
                     f"kernel duration {k_kernel_duration:9.4f} "
                     f"impulse gain {i_impulse_gain:9.4f} "
                     f"dirac level {i_dirac_level:9.4f}"
-            )
+                )
                 setting_number += 1
                 for source_instrument in source_instruments:
                     # Schedule the source instrument.
@@ -127,19 +125,19 @@ def generate_score():
                         chord = chords[i]
                         # print("chord: ", chord)
                         # Schedule the chords.
-                        p4 = k_kernel_duration
-                        p5 = i_impulse_gain
-                        p6 = i_dirac_level
-                        chord_event = [3, time, chord_duration, 1, 1, p4, p5, p6, chord[0], chord[1], chord[2], chord[3]]
+                        chord_event = [3, time, chord_duration, k_kernel_duration, i_impulse_gain, i_dirac_level, chord[0], chord[1], chord[2], chord[3]]
                         # print("chord_event:", chord_event) 
                         score_line = "i " + " ".join(str(x) for x in chord_event)
                         time = time + chord_duration
                         score_lines.append(score_line)
-    score_lines.append(f'i 5 {time} 1')
     return '\n'.join(score_lines)
 score = generate_score()
-print(score)
+csd = csd.replace('''</CsInstruments>''', '''</CsInstruments>\n<CsScore>\n''' + score + '\n</CsScore>\n')
+print(csd)
+with open("from_python.csd", "w") as f:
+    f.write(csd)
+csound.compileCsdText(csd)
+csound.compileCsdText(csd)
 csound.start()
-csound.readScore(score)
 csound.perform()
 
