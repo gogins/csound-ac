9aa40622e5cd52e85df3bd181402886df5cb8270
gogins <michael.gogins@gmail.com>
2026-01-30 12:51:51 +0100

    Added CMake build for ChordSpaceTests (#6).

--- doc/latex.sh
+++ doc/latex.sh
@@ -2,5 +2,6 @@
 echo "Building csound-ac.pdf reference manual..."
 doxygen
 cd latex
-pdflatex --jobname=csound-ac refman.tex
-pdflatex --jobname=csound-ac refman.tex
+rm -f csound-ac.aux csound-ac.toc csound-ac.out csound-ac.lof csound-ac.lot csound-ac.bbl csound-ac.blg csound-ac.run.xml csound-ac.bcf csound-ac.fls csound-ac.fdb_latexmk csound-ac.log   
+pdflatex -jobname=csound-ac refman.tex
+pdflatex -jobname=csound-ac refman.tex
