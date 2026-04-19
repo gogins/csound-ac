436bfa4f770bde15f7c087c79069c42a9533deb9
gogins <michael.gogins@gmail.com>
2026-04-19 12:41:04 -0400

    Updated example for latest Pianoteq.

--- examples/cellular/Cellular-v5-macos.py
+++ examples/cellular/Cellular-v5-macos.py
@@ -266,7 +266,7 @@ nchnls = 2
 
 seed 38493
 
-gi_Pianoteq vst3init "/Library/Audio/Plug-Ins/VST3/Pianoteq 7.vst3", "Pianoteq 7", 1
+gi_Pianoteq vst3init "/Library/Audio/Plug-Ins/VST3/Pianoteq 9.vst3", "Pianoteq 9", 1
 vst3info gi_Pianoteq
 
 alwayson "PianoOutPianoteq"
