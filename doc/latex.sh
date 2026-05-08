aa438d67faf90fc7b4f150206130981cdd2c29bf
gogins <michael.gogins@gmail.com>
2026-05-08 11:12:07 -0400

    Removed PDF docs as too slow to build on runners.

--- doc/latex.sh
+++ doc/latex.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-echo "Building csound-ac.pdf reference manual..."
+echo "Building csound-ac HTML reference manual..."
 doxygen
-cd latex
-rm -f csound-ac.aux csound-ac.toc csound-ac.out csound-ac.lof csound-ac.lot csound-ac.bbl csound-ac.blg csound-ac.run.xml csound-ac.bcf csound-ac.fls csound-ac.fdb_latexmk csound-ac.log   
-pdflatex -jobname=csound-ac refman.tex
-pdflatex -jobname=csound-ac refman.tex
